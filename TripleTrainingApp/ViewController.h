1d9a7657c0fb55f3922434b1b89d5ef9d19a250e
Ken Ortego <kortego@Ken-Ortegos-MBP.attlocal.net>
2015-09-02 21:44:15 -0500

    Initial Commit

---
+++ TripleTrainingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TripleTrainingApp
+//
+//  Created by Ken Ortego on 9/2/15.
+//  Copyright (c) 2015 com.KiloOscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
